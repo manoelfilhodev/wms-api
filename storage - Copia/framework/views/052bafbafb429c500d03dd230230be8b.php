4037f9ca203e44c6d80b92665d7e5104732f1059
Manoel Filho <manoel.filho.mf@icloud.com>
2025-05-06 21:09:12 -0300

    Apagando pasta desnecessária

--- storage - Copia/framework/views/052bafbafb429c500d03dd230230be8b.php
+++
@@ -1,2 +0,0 @@
-<small class="badge badge-<?php echo e($colour); ?>"><?php echo e($text); ?></small>
-<?php /**PATH /var/www/vendor/knuckleswtf/scribe/src/../resources/views//components/badges/base.blade.php ENDPATH**/ ?>
